91f88d9167c5f222fdd3e04f94624bbab935b47c
stormsidali2001 <assoulsidali@gmail.com>
2022-09-06 03:02:05 -0700

    fist commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [AuthModule,UserModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
