4458f5f90e95389cfbfb5d4d04b92e8c07c4560e
stormsidali2001 <assoulsidali@gmail.com>
2022-09-12 09:58:09 -0700

    topbar done...

--- frontend/features/dashboard/components/Topbar/Topbar.tsx
+++ frontend/features/dashboard/components/Topbar/Topbar.tsx
@@ -1,11 +1,39 @@
+import { Badge, IconButton } from '@mui/material'
+import LittleChevronIcon from '../../../../icons/LittleChevronIcon'
+import NotificationIcon from '../../../../icons/NotificationIcon'
 import styles from './Topbar.module.css'
 
 const Topbar = () => {
   return (
     <div className={styles.container}>
-        Topbar
+       <div className={styles.indications}>
+            <span>Tableau de Bord</span>
+            <span>Juridique</span>
+       </div>
+       <div className={styles.rightButtons}>
+
+            <IconButton  aria-label={notificationsLabel(100)}> 
+                <Badge badgeContent={9} sx={{padding:0}}  className={styles.notificationBdge}>
+                    <NotificationIcon/>
+                </Badge>
+            </IconButton>
+            <div className={styles.profilImgContainer}>
+                <img src="sidali.jpg" alt="profile picture"/>
+                <LittleChevronIcon className={styles.chevron}/>
+            </div>
+          
+           
+       </div>
     </div>
   )
 }
-
+function notificationsLabel(count: number) {
+    if (count === 0) {
+      return 'no notifications';
+    }
+    if (count > 99) {
+      return 'more than 99 notifications';
+    }
+    return `${count} notifications`;
+  }
 export default Topbar
---
+++ frontend/icons/LittleChevronIcon.tsx
@@ -0,0 +1,26 @@
+
+const LittleChevronIcon = (props:any) => {
+  return (
+<svg {...props} width="13" height="11" viewBox="0 0 13 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_d_28_49)">
+    <ellipse cx="6.5" cy="3.18182" rx="3.5" ry="3.18182" fill="white"/>
+    </g>
+    <path d="M8.95511 2.21284C8.7457 2.03716 8.44039 2.03718 8.23099 2.21288L6.9365 3.29906C6.71464 3.48522 6.39115 3.48522 6.16929 3.29906L4.8748 2.21288C4.66541 2.03718 4.3601 2.03716 4.15069 2.21284C3.88232 2.43798 3.88233 2.85085 4.15072 3.07597L6.16937 4.76921C6.3912 4.95528 6.71459 4.95528 6.93642 4.76921L8.95508 3.07597C9.22346 2.85085 9.22347 2.43798 8.95511 2.21284Z" fill="#191A1B"/>
+    <defs>
+    <filter id="filter0_d_28_49" x="0.963636" y="0" width="11.0727" height="10.4364" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+    <feOffset dy="2.03636"/>
+    <feGaussianBlur stdDeviation="1.01818"/>
+    <feComposite in2="hardAlpha" operator="out"/>
+    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_28_49"/>
+    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_28_49" result="shape"/>
+    </filter>
+    </defs>
+</svg>
+
+  )
+}
+
+export default LittleChevronIcon
