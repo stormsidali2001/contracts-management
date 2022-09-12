ca9dd2cf60bd8a8f1a1c7322c1a54e5b17f36d2c
stormsidali2001 <assoulsidali@gmail.com>
2022-09-12 05:36:28 -0700

    ...

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,11 @@
 import type { NextPage } from 'next'
-import DashboardContent from '../features/dashboard/components/DashboardContent/DashboardContent'
-import Sidebar from '../features/dashboard/components/Sidebar/Sidebar'
 import styles from '../styles/Home.module.css'
-import Rightside from '../features/dashboard/components/Rightside/Rightside'
+
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.glass}>
-        <Sidebar/>
-        <DashboardContent/>
-        <Rightside/>
-      </div>
+      
     </div>
   )
 }
