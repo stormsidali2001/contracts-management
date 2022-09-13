9056e6bf6b725c7746cefbbde67baf769f681f4e
stormsidali2001 <assoulsidali@gmail.com>
2022-09-13 10:23:26 -0700

    event ba done...

--- frontend/features/dashboard/components/lastEventsCard/LastEventsCard.tsx
+++ frontend/features/dashboard/components/lastEventsCard/LastEventsCard.tsx
@@ -2,7 +2,62 @@ import styles from './LastEventsCard.module.css'
 
 const LastEventsCard = () => {
   return (
-    <div className={styles.container}>LastEventsCard</div>
+    <div className={styles.container}>
+      <div className={styles.title}>Evenements</div>
+      <ul className={styles.events}>
+        <li className={styles.event}>
+            <span  className={`${styles.circle} ${styles.juridicalCircle}`}></span>
+            <p className={styles.body}>nouveau contrat: DRG dp1 
+            dans le departement 
+            </p>
+
+        </li>
+        <li className={styles.event}>
+            <span className={`${styles.circle} ${styles.juridicalCircle}`}></span>
+            <p className={styles.body}>nouveau contrat: DRG dp1 
+            dans le departement 
+            </p>
+
+        </li>
+        <li className={styles.event}>
+            <span className={`${styles.circle} ${styles.adminOrChief}`}></span>
+            <p className={styles.body}>nouveau contrat: DRG dp1 
+            dans le departement 
+            </p>
+
+        </li>
+        <li className={styles.event}>
+            <span className={`${styles.circle} ${styles.adminOrChief}`}></span>
+            <p className={styles.body}>nouveau contrat: DRG dp1 
+            dans le departement 
+            </p>
+        </li>
+        <li className={styles.event}>
+            <span className={`${styles.circle} ${styles.adminOrChief}`}></span>
+            <p className={styles.body}>nouveau contrat: DRG dp1 
+            dans le departement 
+            </p>
+        </li>
+        <li className={styles.event}>
+            <span className={`${styles.circle} ${styles.adminOrChief}`}></span>
+            <p className={styles.body}>nouveau contrat: DRG dp1 
+            dans le departement 
+            </p>
+        </li>
+        <li className={styles.event}>
+            <span className={`${styles.circle} ${styles.adminOrChief}`}></span>
+            <p className={styles.body}>nouveau contrat: DRG dp1 
+            dans le departement 
+            </p>
+        </li>
+        <li className={styles.event}>
+            <span className={`${styles.circle} ${styles.adminOrChief}`}></span>
+            <p className={styles.body}>nouveau contrat: DRG dp1 
+            dans le departement 
+            </p>
+        </li>
+      </ul>
+    </div>
   )
 }
 
