76bc6e00aafa83e76fe722077fa8a35e5824445b
stormsidali2001 <assoulsidali@gmail.com>
2022-09-23 09:20:50 -0700

    vendor page done

--- frontend/features/vendor/components/vendorContent/vendorContent.tsx
+++ frontend/features/vendor/components/vendorContent/vendorContent.tsx
@@ -2,6 +2,7 @@ import styles from './vendorContent.module.css';
 import {useEffect , useState} from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
+import { Button } from '@mui/material';
 const VendorContent = () => {
     const router = useRouter();
     const [vendor,setVendor] = useState<any>(null)
@@ -21,11 +22,52 @@ const VendorContent = () => {
   return (
     <div className={styles.container}>
        <div className={styles.left}>
-            <div className={styles.vendorCard}></div>
+            <div className={styles.vendorCard}>
+              <div className={styles.labelWrapper}>
+                <div className={styles.label}>N</div>
+                <div className={styles.labelText}>{vendor?.num}</div>
+              </div>
+              <div className={styles.vendorTitle}>Fournisseur</div>
+              <div className={styles.content}>
+                <div className={styles.vendorContentItem}>
+                  <span>raison sociale:</span>
+                  <span>{vendor?.company_name}</span>
+                </div>
+                <div className={styles.vendorContentItem}>
+                  <span>nif:</span>
+                  <span>{vendor?.nif}</span>
+                </div>
+                <div className={styles.vendorContentItem}>
+                  <span>nrc:</span>
+                  <span>{vendor?.nrc}</span>
+                </div>
+                <div className={styles.vendorContentItem}>
+                  <span>mobile:</span>
+                  <span>{vendor?.mobile_phone_number}</span>
+                </div>
+                <div className={styles.vendorContentItem}>
+                  <span>fixe:</span>
+                  <span>{vendor?.home_phone_number}</span>
+                </div>
+                <div className={styles.vendorContentItem}>
+                  <span>adresse:</span>
+                  <span>{vendor?.address}</span>
+                </div>
+              </div>
+            </div>
+            
        </div>
        <div className={styles.right}>
-            <div className={styles.littleCard}></div>
-            <div className={styles.littleCard}></div>
+            <div className={styles.littleCard}>
+              <div className={styles.title}>Contrat</div>
+              <div className={styles.counter}>{vendor?.contractCount}</div>
+              <Button color="secondary" className={styles.cardButton} variant="contained">voir</Button>
+            </div>
+            <div className={styles.littleCard}>
+              <div className={styles.title}>Convension</div>
+              <div className={styles.counter}>{vendor?.convensionCount}</div>
+              <Button color="secondary" className={styles.cardButton}  variant="contained">voir</Button>
+            </div>
        </div>
     </div>
   )
