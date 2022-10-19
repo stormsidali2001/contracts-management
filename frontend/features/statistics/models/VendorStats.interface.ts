1b602ac7ba0d14f92e842de7353bf4293082f79d
stormsidali2001 <assoulsidali@gmail.com>
2022-10-19 02:47:30 -0700

    .....

---
+++ frontend/features/statistics/models/VendorStats.interface.ts
@@ -0,0 +1,7 @@
+interface VendorStat{
+    id:string;
+    date:Date;
+    nb_vendors:number;
+}
+
+export type VendorStats = VendorStat[];
---
+++ frontend/features/statistics/services/stats.service.ts
@@ -0,0 +1,8 @@
+function getStats(){
+
+}
+
+
+
+
+export {getStats};
