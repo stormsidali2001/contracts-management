5a1496c5a100893fe92cc36c1a082dd4a53f2f71
stormsidali2001 <assoulsidali@gmail.com>
2022-10-19 10:43:31 -0700

    user stats are online but creating a new vendor causing the socket to disconnect

--- frontend/features/statistics/StatisticsSlice.ts
+++ frontend/features/statistics/StatisticsSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { AgreementStatus } from "./models/AgreementStats.interface";
 import { TopDirections } from "./models/AgreementsTopDirections.interface";
 import { AgreementTypes } from "./models/AgreementTypes.interface";
@@ -6,6 +6,9 @@ import { UserTypes } from "./models/UserTypes.interface";
 import { VendorStats } from "./models/VendorStats.interface";
 import statisticsService from "./services/stats.service";
 import {AxiosInstance} from 'axios'
+import { UserRole } from "../auth/models/user-role.enum";
+import { Entity } from "../notification/models/Entity.enum";
+import { Operation } from "../notification/models/Operation.enum";
 
 interface AsyncState{
     isSuccess:boolean;
@@ -44,7 +47,52 @@ const StatisticsSlice = createSlice(
         name:"StatisticsSlice",
         initialState,
         reducers:{
+            newCreatedUserEvent:(state,action:PayloadAction<{type:Entity,operation:Operation}>)=>{
+                const value  =   action.payload.type.toLocaleLowerCase() as ("admin" |"employee" | "juridical")
+                if(state.userTypes){
+                    if(action.payload.operation === Operation.DELETE){
+                        if(state.userTypes[value] > 0 ) state.userTypes[value] -= 1;
+                        if(state.userTypes.total > 0 ) state.userTypes.total -= 1;
 
+                    }else if(action.payload.operation === Operation.INSERT){
+                        
+                        state.userTypes[value] += 1;
+                        state.userTypes.total += 1;
+                    }
+                }
+            },
+            newCreatedVendorEvent:(state,action:PayloadAction<{date:Date,operation:Operation}>)=>{
+                if(!state.vendorsStats) return;
+                const index = state.vendorsStats.findIndex(v=>{
+
+                    v.date === action.payload.date
+
+                    const a_d = v.date;
+                    const b_d = action.payload.date;
+                    a_d.setHours(0,0,0,0)
+                    b_d.setHours(0,0,0,0)
+
+                    return Number(a_d) === Number(b_d);
+                })
+                // if(index > 0){
+                    
+                //     if(action.payload.operation === Operation.DELETE){
+                //         state.vendorsStats[index].nb_vendors -= 1;
+                //     }else if(action.payload.operation === Operation.INSERT){
+                //         state.vendorsStats[index].nb_vendors += 1;
+                //     }
+                    
+                // }else{
+                //     state.vendorsStats.unshift({date:action.payload.date,nb_vendors:1,id:new Date().toString()})
+                //     state.vendorsStats = state.vendorsStats.sort((a,b)=>{
+                //         const a_d = a.date;
+                //         const b_d = b.date;
+                //         a_d.setHours(0,0,0,0)
+                //         b_d.setHours(0,0,0,0)
+                //        return  Number(b_d) - Number(a_d)
+                //     })
+                // }
+            }
         },
         extraReducers:(builder)=>{
             builder.addCase(getStatistics.pending,(state,action)=>{
@@ -70,6 +118,6 @@ const StatisticsSlice = createSlice(
     }
 )
 
-export const actions = StatisticsSlice.actions;
+export const {newCreatedUserEvent,newCreatedVendorEvent} = StatisticsSlice.actions;
 
 export default StatisticsSlice;
