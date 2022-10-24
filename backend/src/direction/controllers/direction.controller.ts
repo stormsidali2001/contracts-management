4697e745c8f5920986fd9e3b53527d1bc62416db
Houda-DE <houdadebza8@gmail.com>
2022-10-24 11:47:16 -0700

    ...

--- backend/src/direction/controllers/direction.controller.ts
+++ backend/src/direction/controllers/direction.controller.ts
@@ -1,9 +1,12 @@
-import {Controller,Post,Body, Query, Get, ParseIntPipe, Delete,Param,Put} from '@nestjs/common';
+import {Controller,Post,Body, Query, Get, ParseIntPipe, Delete,Param,Put, UseGuards} from '@nestjs/common';
 import { CreateDirectionDTO, updateDirectionDTO } from 'src/core/dtos/direction.dto';
 import { DirectionEntity } from 'src/core/entities/Direction.entity';
 import { UpdateResult } from 'typeorm';
 import { DirectionService } from '../services/direction.service';
 import {ApiTags} from '@nestjs/swagger';
+import { JwtAccessTokenGuard } from 'src/auth/guards/jwt-access-token.guard';
+import { RoleGuard } from 'src/auth/guards/Role.guard';
+import { RequiredRoles } from 'src/auth/decorators/RequiredRoles.decorator';
 
 @ApiTags('directions')
 @Controller('directions')
@@ -12,6 +15,8 @@ export class DirectionController{
         private readonly directionService:DirectionService
     ){}
 
+    // @RequiredRoles('admin')
+    // @UseGuards(JwtAccessTokenGuard,RoleGuard)
     @Post('')
     async createDirection(@Body() direction:CreateDirectionDTO){
         console.log("...........................",JSON.stringify(direction))
@@ -21,6 +26,8 @@ export class DirectionController{
     async findAll(@Query('offset') offset:number ,@Query('limit') limit:number ):Promise<DirectionEntity[]>{
         return await this.directionService.findAll(offset,limit);
     }   
+    // @RequiredRoles('admin')
+    // @UseGuards(JwtAccessTokenGuard,RoleGuard)
     @Delete(':id')
     async deleteDirection(@Param('id') id:string): Promise<string>{
         return await this.directionService.deleteDirection(id);
