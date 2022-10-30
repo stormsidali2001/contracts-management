43005fb1dcbad0e7df44e1763d5c715cb23e8f3e
stormsidali2001 <assoulsidali@gmail.com>
2022-10-30 15:32:28 +0100

    adding a get stats intervalle reducer in the stats slice

--- frontend/features/statistics/services/stats.service.ts
+++ frontend/features/statistics/services/stats.service.ts
@@ -1,12 +1,20 @@
 import { AxiosInstance } from "axios";
 import axios from "../../../api/axios";
 
-async function getStatistics({axiosInstance}:{axiosInstance:AxiosInstance}){
+async function getStatistics({axiosInstance,startDate,endDate}:{axiosInstance:AxiosInstance,startDate?:string,endDate?:string}){
+  const body:{startDate?:string,endDate?:string} = {};
+  if(startDate ){
+    body.startDate = startDate;
+
+   }
+   if(endDate){
+    body.endDate = endDate;
+   }
+
     const res =  await axios.get('/statistics') 
     if(!res.data) throw new Error("empty data statisticsService/getStatistics")
     return res.data;
 }
-
 
 
 
